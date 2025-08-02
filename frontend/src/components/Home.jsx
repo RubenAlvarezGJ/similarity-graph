7178a8a967e0beac525a8592770c4bbc5a30a5be
Ruben Alvarez-Gutierrez <ruben513@ucsb.edu>
2025-08-02 02:25:26 -0700

    home component

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    navigate("/loading");
+    navigate("/loading", { state: { url } });
 
     try {
       const response = await fetch(`/api/crawl?url=${encodeURIComponent(url)}`);
@@ -20,16 +20,26 @@ function Home() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="centered">
-      <h2>Enter a URL</h2>
-      <input
-        type="text"
-        value={url}
-        onChange={(e) => setUrl(e.target.value)}
-        placeholder="https://example.com"
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <div className="bg-dot-grid flex flex-col items-center justify-center h-screen gap-4 text-white">
+      <form onSubmit={handleSubmit} className="-mt-70 flex flex-col gap-3">
+        <div className="flex flex-col items-center gap-4">
+          <h1 className="text-5xl font-semibold">Enter a URL</h1>
+          <input
+            type="text"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+            placeholder="https://example.com"
+            className="border border-white-300 rounded-lg px-3 py-2 w-100"
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-white text-black px-4 py-2 rounded-lg hover:bg-purple-900"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
   );
 }
 

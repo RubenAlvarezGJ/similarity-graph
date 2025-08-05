d884c48988c9f4f0bc300ed16a80b74f22ce56e3
Ruben Alvarez-Gutierrez <ruben513@ucsb.edu>
2025-08-05 02:13:22 -0700

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,12 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://similarity-graph-backend.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
